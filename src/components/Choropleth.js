f99f89ea9b04d5f396b5152022853c83080e06ae
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-08-09 16:26:17 -0700

    Choropleth component now passes the current plot container size into the choroplethBuilder function when calling it
    Choropleth component now also returns plot title and description elements

--- src/components/Choropleth.js
+++ src/components/Choropleth.js
@@ -4,14 +4,33 @@ import choroplethBuilder from './helpers/choroplethBuilder';
 
 const choroContainerId = 'choropleth-container';
 
-export default function Choropleth({ educationData, countyTopoData }) {
-  // On mount, run d3 script to build graph:
+export default function Choropleth({
+  educationData,
+  countyTopoData,
+  containerWidth,
+  setContainerOpacity,
+}) {
+  // On mount, run d3 script to build graph, then make container visible:
   useEffect(() => {
-    choroplethBuilder(educationData, countyTopoData, `#${choroContainerId}`);
-  }, []);
+    choroplethBuilder(
+      educationData,
+      countyTopoData,
+      `#${choroContainerId}`,
+      containerWidth,
+    );
+
+    setContainerOpacity(100);
+  }, [educationData, countyTopoData, containerWidth, setContainerOpacity]);
+
   return (
     <>
-      <h1>This is the choropleth</h1>
+      <h1 id="title" className="display-6">
+        United States Educational Attainment Choropleth
+      </h1>
+      <h3 id="description" className="display-6">
+        Percentage of adults age 25 and older with a bachelor's degree or higher
+        (2010-2014)
+      </h3>
       <div id={choroContainerId} />
     </>
   );
